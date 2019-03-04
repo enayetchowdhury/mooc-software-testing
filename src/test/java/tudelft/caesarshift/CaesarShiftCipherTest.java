6cfc392f3ffd553b7f65cdeaf0b7b026ec9bdf3c
Enayet Chowdhury <enayet@Enayets-iMac.local>
2019-03-04 12:11:26 +0600

    Test has been done on CaeserShiftCipher class

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,17 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
+
+
+    @ParameterizedTest(name = "Cipher={0}, Shift={1}, Result={2}")
+    @CsvSource({"abc,3,def", "xyz,3,abc", "xyz,-3,uvw", "a%c,3,invalid", "efghi,5,jklmn"})
+    public void cipherTest(String str, int shift, String expectedResult) {
+        String code = new CaesarShiftCipher().CaesarShiftCipher(str,shift);
+        Assertions.assertEquals(expectedResult, code);
+    }
 
 }
