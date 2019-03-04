3424bcb822bc2f5d1051144c84797fca671e81f6
Enayet Chowdhury <enayet@Enayets-iMac.local>
2019-03-04 11:42:14 +0600

    gHappy class has been tested

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,17 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "String={0}, Result={1}")
+    @CsvSource({"xxggxx,true", "yyggxx,ture", "xxggyy,true"})
+
+    public void gHappyTest(String str, boolean expectedResult){
+        boolean ans = new GHappy().gHappy(str);
+        Assertions.assertEquals(true, ans);
+    }
 }
